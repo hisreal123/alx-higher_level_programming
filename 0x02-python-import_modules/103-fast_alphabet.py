9de1d491385a95d19fa6a900fce89b5fcd8bda3c
hisreal123 <omosgoerge@gmail.com>
2023-12-01 16:42:02 +0100

    end

--- 0x02-python-import_modules/103-fast_alphabet.py
+++ 0x02-python-import_modules/103-fast_alphabet.py
@@ -1,4 +1,3 @@
 #!/usr/bin/python3
-if __name__ == "__main__":
-    from string import ascii_uppercase as alphabet
-    print(alphabet)
+from string import ascii_uppercase as alphabet
+print(alphabet)
