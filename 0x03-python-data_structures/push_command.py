66e2c412030d2e9113ff8eaaa75462c376afd8dd
hisreal123 <omosgoerge@gmail.com>
2023-12-03 00:38:46 +0100

    more automation

--- 0x03-python-data_structures/push_command.py
+++ 0x03-python-data_structures/push_command.py
@@ -4,12 +4,19 @@ import sys
 if __name__ == "__main__":
     import subprocess
 
+    # git add .
     subprocess.run(['git', 'add', '.'])
     print("All new files successfully added")
 
+    # git commit
     commit_message = input('Enter commit message: ')
     if not commit_message:
         print('You have no entered commit message nigga !!')
     else:
         subprocess.run(['git', 'commit', '-m', commit_message])
         print("successfully committed !!: '{}'".format(commit_message))
+
+    username = 'hisreal123'
+    userToken = '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'
+
+    subprocess.run(['git', 'push'])
