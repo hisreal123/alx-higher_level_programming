4b4f7539e91e308c3a132277bb30d37078f27892
hisreal123 <omosgoerge@gmail.com>
2023-12-03 00:21:21 +0100

    alx problem

--- 0x03-python-data_structures/0-print_list_integer.py
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
-def print_list_integer(my_list = []):
+def print_list_integer(my_list=None):
     if my_list is None:
         my_list = []
     for i in my_list:
-        print("{}".format(int(i)))
+        print("{}".format(int))
