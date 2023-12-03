0f192bf95f05742196651fd02046c796657f33de
hisreal123 <omosgoerge@gmail.com>
2023-12-03 01:50:51 +0100

    please work !!!!

--- 0x03-python-data_structures/push_command.py
+++ 0x03-python-data_structures/push_command.py
@@ -7,21 +7,19 @@ if __name__ == "__main__":
     user_token = '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'
     repo = 'git@github.com:hisreal123/alx-higher_level_programming.git'
 
-    # git add .
-    if subprocess.run(['git', 'add', '.']):
-        print("All new files successfully added")
+    subprocess.run(['git', 'add', '.'])
+    print("All new files successfully added")
 
     # git commit
     commit_message = input('Enter commit message: ')
     if not commit_message:
         print('You have not entered a commit message!')
     else:
-
         subprocess.run(['git', 'commit', '-m', commit_message])
         print("Successfully committed: '{}'".format(commit_message))
 
-        # Set the Git credential helper to cache credentials
-        subprocess.run(['git', 'config', '--global', 'credential.helper', 'cache'])
+        # Set the Git credential helper to use libsecret
+        subprocess.run(['git', 'config', '--global', 'credential.helper', 'libsecret'])
 
         # git push with the personal access token
         subprocess.run(['git', 'push', 'origin', 'main'])
