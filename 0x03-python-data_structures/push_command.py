a30c60f38f116b32e75fd291ef3ec0826f6196ea
hisreal123 <omosgoerge@gmail.com>
2023-12-03 01:31:38 +0100

    maybe will work !!!

--- 0x03-python-data_structures/push_command.py
+++ 0x03-python-data_structures/push_command.py
@@ -1,15 +1,7 @@
 #!/usr/bin/python3
 import subprocess
-import os
 
 if __name__ == "__main__":
-    username = 'hisreal123'
-    user_token = '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'
-    repo = 'git@github.com:hisreal123/alx-higher_level_programming.git'
-
-    # Make sure SSH agent is running and add the key
-    subprocess.run(['ssh-add', '/home/treasure/.ssh/id_ed25519'])
-
     # git add .
     subprocess.run(['git', 'add', '.'])
     print("All new files successfully added")
@@ -22,7 +14,6 @@ if __name__ == "__main__":
         subprocess.run(['git', 'commit', '-m', commit_message])
         print("Successfully committed: '{}'".format(commit_message))
 
-        env = os.environ.copy()
-        env["GIT_SSH_COMMAND"] = "ssh -o 'StrictHostKeyChecking=no'"
-        subprocess.run(['git', 'push', 'origin', 'main'], env=env)
+        # git push with the saved token
+        subprocess.run(['git', 'push', 'origin', 'main'])
         print('Git push successful!')
