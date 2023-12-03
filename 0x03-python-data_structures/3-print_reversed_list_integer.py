c89c4cda955cad4d38a64d27c63a96388770c4e1
hisreal123 <omosgoerge@gmail.com>
2023-12-03 22:30:21 +0100

    fix ahead reverse list

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 def print_reversed_list_integer(my_list=None):
-    for i in my_list[::-1]:
-        int(i)
-        print(i)
+    reversed_list = my_list[::-1]
+    for i in reversed_list:
+        print("{}".format(int(i)))
