df96e97de9c539a9e58e924a297c68f7069c5efc
hisreal123 <omosgoerge@gmail.com>
2023-12-03 01:21:35 +0100

    Hey

--- 0x03-python-data_structures/push_command.py
+++ 0x03-python-data_structures/push_command.py
@@ -1,16 +1,26 @@
 #!/usr/bin/python3
-import sys
+import subprocess
+import os
 
 if __name__ == "__main__":
-    import subprocess
-    import os
-
     username = 'hisreal123'
     user_token = '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'
     repo = 'git@github.com:hisreal123/alx-higher_level_programming.git'
 
     # Start SSH agent and add key
-    subprocess.run(['eval', '$(ssh-agent -s)'], shell=True)
+    ssh_agent_process = subprocess.Popen(['eval', '$(ssh-agent -s)'], stdout=subprocess.PIPE, shell=True)
+    ssh_agent_output = ssh_agent_process.communicate()[0]
+
+    # Extract the SSH_AUTH_SOCK and SSH_AGENT_PID values
+    for line in ssh_agent_output.splitlines():
+        if b'SSH_AUTH_SOCK' in line:
+            env_key, env_value = line.split(b'=')
+            os.environ[env_key.decode()] = env_value.decode()
+        if b'SSH_AGENT_PID' in line:
+            env_key, env_value = line.split(b'=')
+            os.environ[env_key.decode()] = env_value.decode()
+
+    # Add the SSH key to the agent
     subprocess.run(['ssh-add', '/home/treasure/.ssh/id_ed25519'])
 
     # git add .
@@ -20,10 +30,10 @@ if __name__ == "__main__":
     # git commit
     commit_message = input('Enter commit message: ')
     if not commit_message:
-        print('You have no entered commit message nigga !!')
+        print('You have not entered a commit message!')
     else:
         subprocess.run(['git', 'commit', '-m', commit_message])
-        print("successfully committed !!: '{}'".format(commit_message))
+        print("Successfully committed: '{}'".format(commit_message))
 
         env = os.environ.copy()
         env["GIT_SSH_COMMAND"] = "ssh -o 'StrictHostKeyChecking=no'"
