096090afe8b425e8606b625326c95db0d329278d
hisreal123 <omosgoerge@gmail.com>
2023-12-03 01:08:08 +0100

    fixed again with ssh

--- 0x03-python-data_structures/push_command.py
+++ 0x03-python-data_structures/push_command.py
@@ -6,7 +6,7 @@ if __name__ == "__main__":
 
     username = 'hisreal123'
     user_token = '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'
-    repo = f'https://{username}:{user_token}@github.com/hisreal123/alx-higher_level_programming.git'
+    repo = 'git@github.com:hisreal123/alx-higher_level_programming.git'
 
     # git add .
     subprocess.run(['git', 'add', '.'])
@@ -20,5 +20,5 @@ if __name__ == "__main__":
         subprocess.run(['git', 'commit', '-m', commit_message])
         print("successfully committed !!: '{}'".format(commit_message))
 
-        subprocess.run(['git', 'push', '-u', 'origin', 'main'])
+        subprocess.run(['git', 'push', 'origin', 'main'], env={"GIT_SSH_COMMAND": "ssh -o 'StrictHostKeyChecking=no'"})
         print('Git push successful!')
