08ceda9d047709d9ed2570f03ca24002555f0fc1
hisreal123 <omosgoerge@gmail.com>
2023-12-04 19:04:18 +0100

    Working i guess

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python3
 
 def no_c(my_string):
-    new_str = ''
+    new_str = " "
     for char in my_string:
-        if char != 'c' and char != 'C':
+        if char != "c" and char != "C":
             new_str += char
-    return (new_str)
+    return new_str
