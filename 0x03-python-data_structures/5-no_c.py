b5caf1aa9baf3908e374c0d59cbcae1c56370e47
hisreal123 <omosgoerge@gmail.com>
2023-12-04 14:53:38 +0100

    Fixed again

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,5 +1,8 @@
 #!/usr/bin/env python3
 def no_c(my_string):
+    result = ""
     for i in my_string:
         if i != "c" and i != "C":
-            print("{}".format(i), end="")
+            result += i
+
+    return result
