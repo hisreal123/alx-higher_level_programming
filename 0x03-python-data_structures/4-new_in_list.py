f49583824ee4f43d0d4dde6ee96101fd3bd51372
hisreal123 <omosgoerge@gmail.com>
2023-12-04 11:01:11 +0100

    Fixed this also

---
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+def new_in_list(my_list, idx, element):
+    if idx < 0 or idx >= len(my_list):
+        copy_list = my_list[:]
+        copy_list[idx] = element
+        return copy_list
+        # or
+        # return my_list.copy()
+
+    else:
+
+        new_list = my_list[:]
+        new_list[idx] = element
+
+        return new_list
