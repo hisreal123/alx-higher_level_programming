61a388f9074a7edfad83fa024c36331c52d332fb
hisreal123 <omosgoerge@gmail.com>
2023-12-04 19:32:50 +0100

    push jare

---
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+def print_matrix_integer(matrix=None):
+    if not matrix or not matrix[0]:
+        return
+
+    for row in matrix:
+        for i, num in enumerate(row):
+            if i == len(row) - 1:
+                print("{:d}".format(num), end="")
+            else:
+                print("{:d} ".format(num), end="")
+        print()
