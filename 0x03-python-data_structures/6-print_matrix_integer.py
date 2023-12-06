652afc55d0b2432cef98889c8fec69f0e2e3d3c8
hisreal123 <omosgoerge@gmail.com>
2023-12-06 12:32:44 +0100

    Tuple wahala

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -2,7 +2,6 @@
 def print_matrix_integer(matrix=None):
     if not matrix or not matrix[0]:
         return
-
     for row in matrix:
         for i, num in enumerate(row):
             if i == len(row) - 1:
---
+++ 0x03-python-data_structures/tp_test.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+tuple_a = (2, 3)
+tuple_b = (4, )
+
+print(len(tuple_b))
+# print(add_tuple(tuple_a, (1, )))
+# print(add_tuple(tuple_a, ()))
