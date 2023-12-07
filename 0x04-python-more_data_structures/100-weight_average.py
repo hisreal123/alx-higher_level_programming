90bfafd23b2b5efc598c0b0a8a16d4cc18efa4de
hisreal123 <omosgoerge@gmail.com>
2023-12-07 02:20:38 +0100

    s

--- 0x04-python-more_data_structures/100-weight_average.py
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -1,7 +1,10 @@
 #!/usr/bin/python3
-weight_average = __import__('100-weight_average').weight_average
-
-my_list = [(1, 2), (2, 1), (3, 10), (4, 2)]
-# = ((1 * 2) + (2 * 1) + (3 * 10) + (4 * 2)) / (2 + 1 + 10 + 2)
-result = weight_average(my_list)
-print("Average: {:0.2f}".format(result))
+def weight_average(my_list=None):
+    if not my_list:
+        return 0
+    average = 0
+    div = 0
+    for tup in my_list:
+        average += tup[0] * tup[1]
+        div += tup[1]
+    return float(average / div)
