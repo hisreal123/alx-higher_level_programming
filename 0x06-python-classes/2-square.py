78a9b45323d48aac3175034c410ec9c8b87d451f
hisreal123 <omosgoerge@gmail.com>
2023-12-21 01:52:50 +0100

    Task1 done

---
+++ 0x06-python-classes/2-square.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""Square class definition"""
+
+
+class Square:
+    """Represent a square"""
+
+    def __init__(self, size):
+        """initialize  a new Square.
+        Args:
+            size (int): The size of the new square"""
+
+        self.__size = size
