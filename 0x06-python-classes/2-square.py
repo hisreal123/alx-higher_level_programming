ff5431bf64a183a66abfa6fc804b81265e5677ff
hisreal123 <omosgoerge@gmail.com>
2023-12-21 02:08:13 +0100

    Task1 done

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -5,9 +5,13 @@
 class Square:
     """Represent a square"""
 
-    def __init__(self, size):
+    def __init__(self, size=0):
         """initialize  a new Square.
         Args:
-            size (int): The size of the new square"""
-
+            size (int): The size of the new square."""
         self.__size = size
+
+        if int(size):
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
