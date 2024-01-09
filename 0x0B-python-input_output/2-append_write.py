fd80db6bf3df4ca4009f0031ba522f9809811148
hisreal123 <omosgoerge@gmail.com>
2024-01-09 15:52:21 -0600

    s

--- 0x0B-python-input_output/2-append_write.py
+++ 0x0B-python-input_output/2-append_write.py
@@ -1,1 +1,16 @@
+#!/usr/bin/python3
+"""Defines read file module """
 
+
+def append_write(filename="", test=""):
+    """
+    read_file function
+    reads a text file (UTF8) and prints it result to stdout
+    """
+    with open(filename, 'r', encoding='utf-8') as f:
+        number_of_line = f.read()
+
+    number_of_line += test
+
+    with open(filename, 'w', encoding='utf-8') as file:
+        return file.write(number_of_line)
