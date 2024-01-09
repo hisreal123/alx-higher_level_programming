5066f62b33fc2578147d1fbf0ef6dedc46d4cb00
hisreal123 <omosgoerge@gmail.com>
2024-01-09 14:46:30 -0600

    Adding everything with one command

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,5 +1,10 @@
 #!/usr/bin/python
-def read_file(filename=""):
-    with open(filename, encoding="utf-8") as f:
-        return f.read()
 
+"""  function read_file reads a file and return's the value """
+
+
+def read_file(filename=""):
+    """ within the function body"""
+    with open(filename, encoding="utf-8") as f:
+        """ this opens the file, in encoding utf-8 which means it should be readable"""
+        return f.read()
