8039304fef35eb76d1c2d6117d63bc2019bf6e1d
hisreal123 <omosgoerge@gmail.com>
2024-01-09 15:18:27 -0600

    d

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -1,1 +1,11 @@
-#!/usr/bin/python
+#!/usr/bin/python3
+"""Defines write file module """
+
+
+def read_file(filename="", test=""):
+    """
+    write_file function
+    write a text file from an (UTF8)
+    """
+    with open(filename, mode='w', encoding='utf-8') as f:
+        return f.write(test)
