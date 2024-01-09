4ce63e99cb10930a5e73d9437666dc63704c2daa
hisreal123 <omosgoerge@gmail.com>
2024-01-09 15:56:12 -0600

    s

--- 0x0B-python-input_output/2-append_write.py
+++ 0x0B-python-input_output/2-append_write.py
@@ -3,10 +3,7 @@
 
 
 def append_write(filename="", test=""):
-    """
-    read_file function
-    reads a text file (UTF8) and prints it result to stdout
-    """
+
     """read_file function reads a text file(UTF8) and prints it result to stdout """
     with open(filename, 'a', encoding='utf-8') as file:
         return file.write(test)
