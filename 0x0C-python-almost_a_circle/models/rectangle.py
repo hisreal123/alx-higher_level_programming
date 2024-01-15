5c40cf8746cb63fdc39236484a521ece94ed6c07
hisreal123 <omosgoerge@gmail.com>
2024-01-15 16:51:35 -0600

    rectangle reformat

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -1,171 +1,25 @@
 #!/usr/bin/python3
+"""Rectangle class implement Base class """
+
 
 from models.base import Base
 
-"""class called Rectangle"""
 
-
-class Rectangle(Base):  # !/usr/bin/python3
+class Rectangle(Base):
+    """ rectangle class implement Base class
     """
-    Class body
-    """
-
-
-import json
-import csv
-import turtle
-
-
-class Base:
-    """Base class body.
-    """
-
-    __nb_objects = 0
-
-    def __init__(self, id=None):
-        """Initialize Id in a constructor
-        """
-        self.__y = None
-        if id is not None:
-            self.id = id
-        else:
-            Base.__nb_objects += 1
-            self.id = Base.__nb_objects
-
-    @staticmethod
-    def to_json_string(list_dictionaries):
-        """ a static method that converts result to Json
-        """
-        if list_dictionaries is None or list_dictionaries == []:
-            return []
-        return json.dumps(list_dictionaries)
-
-    @classmethod
-    def save_to_file(cls, list_objs):
-        """Save list object to JSON
-        """
-        filename = cls.__name__ + ".json"
-        with open(filename, "w") as jsonfile:
-            if list_objs is None:
-                jsonfile.write("[]")
-            else:
-                list_dicts = [o.to_dictionary() for o in list_objs]
-                jsonfile.write(Base.to_json_string(list_dicts))
-
-    @staticmethod
-    def from_json_string(json_string):
-        """Return the deserialization of a JSON string.
-        """
-        if json_string is None or json_string == "[]":
-            return []
-        return json.loads(json_string)
-
-    @classmethod
-    def create(cls, **dictionary):
-        """Return a class instantiated from a dictionary of attributes.
-        """
-        if dictionary and dictionary != {}:
-            if cls.__name__ == "Rectangle":
-                new = cls(1, 1)
-            else:
-                new = cls(1)
-            new.update(**dictionary)
-            return new
-
-    @classmethod
-    def load_from_file(cls):
-        """Return a list of classes instantiated from a file of JSON  string.
-        """
-        filename = str(cls.__name__) + ".json"
-        try:
-            with open(filename, "r") as jsonfile:
-                list_dicts = Base.from_json_string(jsonfile.read())
-                return [cls.create(**d) for d in list_dicts]
-        except IOError:
-            return []
-
-    @classmethod
-    def save_to_file_csv(cls, list_objs):
-        """Write the CSV serialization of a list of objects to a file.
-        """
-        filename = cls.__name__ + ".csv"
-        with open(filename, "w", newline="") as csvfile:
-            if list_objs is None or list_objs == []:
-                csvfile.write("[]")
-            else:
-                if cls.__name__ == "Rectangle":
-                    fieldnames = ["id", "width", "height", "x", "y"]
-                else:
-                    fieldnames = ["id", "size", "x", "y"]
-                writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
-                for obj in list_objs:
-                    writer.writerow(obj.to_dictionary())
-
-    @classmethod
-    def load_from_file_csv(cls):
-        """Return a list of class instantiated from a CSV file.
-        """
-        filename = cls.__name__ + ".csv"
-        try:
-            with open(filename, "r", newline="") as csvfile:
-                if cls.__name__ == "Rectangle":
-                    fieldnames = ["id", "width", "height", "x", "y"]
-                else:
-                    fieldnames = ["id", "size", "x", "y"]
-                list_dicts = csv.DictReader(csvfile, fieldnames=fieldnames)
-                list_dicts = [dict([k, int(v)] for k, v in d.items())
-                              for d in list_dicts]
-                return [cls.create(**d) for d in list_dicts]
-        except IOError:
-            return []
-
-    @staticmethod
-    def draw(cls, list_rectangles, list_squares):
-        """Draw Rectangles and Squares using the turtle module.
-        """
-        window = turtle.Screen()
-        pen = turtle.Pen()
-        figures = list_rectangles + list_squares
-
-        for fig in figures:
-            pen.up()
-            pen.goto(fig.x, fig.y)
-            pen.down()
-            pen.forward(fig.width)
-            pen.right(90)
-            pen.forward(fig.height)
-            pen.right(90)
-            pen.forward(fig.width)
-            pen.right(90)
-            pen.forward(fig.height)
-            pen.right(90)
-
-        window.exitonclick()
-
-    """ class body
-    __width, __height, __x, __y are all private class attribute
-     """
-
-    """Private class attribute"""
-
-    # __width = 0
-    # __height =
 
     def __init__(self, width, height, x=0, y=0, id=None):
-        """
-        def ___init__ is a class constructor
-        :params: width, height, x=0, y=0, id=None
-        """
-        super().__init__(id)
         self.width = width
         self.height = height
         self.x = x
         self.y = y
+        super().__init__(id)
 
     @property
     def width(self):
         """
-            Returning private attribute (__width)
+            Returning private attribute (__width) only
         """
         return self.__width
 
@@ -255,16 +109,6 @@ class Base:
         except IndexError:
             pass
 
-    def to_dictionary(self):
-        """
-            Returns a dictionary representation of this class
-        """
-        return {'x': getattr(self, "x"),
-                'y': getattr(self, "y"),
-                'id': getattr(self, "id"),
-                'height': getattr(self, "height"),
-                'width': getattr(self, "width")}
-
     @staticmethod
     def setter_validation(attribute, value):
         if type(value) != int:
@@ -278,3 +122,14 @@ class Base:
     def __str__(self):
         return "[Rectangle] ({}) {}/{} - {}/{}".format(self.id, self.x, self.y,
                                                        self.width, self.height)
+
+    def to_dictionary(self):
+        """
+            Returns a dictionary representation of this class
+        """
+        return {'x': getattr(self, "x"),
+                'y': getattr(self, "y"),
+                'id': getattr(self, "id"),
+                'height': getattr(self, "height"),
+                'width': getattr(self, "width")}
+
