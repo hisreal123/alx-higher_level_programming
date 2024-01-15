a9a67057f2c7d50c619662e8143fdf6a7c0a1480
hisreal123 <omosgoerge@gmail.com>
2024-01-15 16:24:20 -0600

    fixed rectangle to the tests

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -28,65 +28,117 @@ class Rectangle(Base):
 
     @property
     def width(self):
-        """Getter for width"""
+        """
+            Returning private attribute (__width)
+        """
         return self.__width
 
     @width.setter
     def width(self, value):
-        """Setter for width"""
-        if not isinstance(value, int):
-            raise TypeError("Width must be an integer.")
-        if value <= 0:
-            raise ValueError("Width must be > 0.")
+        """
+            Setting private attribute (__width)
+        """
+        self.setter_validation("width", value)
         self.__width = value
 
     @property
     def height(self):
-        """Getter for height"""
+        """
+            Returning private attribute (___height)
+        """
         return self.__height
 
     @height.setter
     def height(self, value):
-        """Setter for height"""
-        if not isinstance(value, int):
-            raise TypeError("Height must be an integer.")
-        if value <= 0:
-            raise ValueError("Height must be > 0.")
+        """
+            Setting private attribute (__height)
+        """
+        self.setter_validation("height", value)
         self.__height = value
 
     @property
     def x(self):
-        """Getter for x"""
+        """
+            Returning private attribute (__x)
+        """
         return self.__x
 
     @x.setter
     def x(self, value):
-        """Setter for x"""
-        if not isinstance(value, int):
-            raise TypeError("X must be an integer.")
-        if value < 0:
-            raise ValueError("X must be >= 0.")
+        """
+            Setting private attribute (__x)
+        """
+        self.setter_validation("x", value)
         self.__x = value
 
     @property
     def y(self):
-        """Getter for y"""
+        """
+            Returning private attribute (__y)
+        """
         return self.__y
 
     @y.setter
     def y(self, value):
-        """Setter for y"""
-        if not isinstance(value, int):
-            raise TypeError("Y must be an integer.")
-        if value < 0:
-            raise ValueError("Y must be >= 0.")
+        """
+            Setting private attribute (__y)
+        """
+        self.setter_validation("y", value)
         self.__y = value
 
     def area(self):
-        """Calculate and return the area of the rectangle"""
-        return self.height * self.width
+        """
+            Returns the area of the rectangle (height * width)
+        """
+        return (self.height * self.width)
 
     def display(self):
-        """Print the Rectangle instance using '#3 characters"""
-        for _ in range(self.height):
-            print('#' * self.width)
+        """
+            Prints to stdout the representation of the rectangle
+        """
+        rectangle = ""
+        print("\n" * self.y, end="")
+        for i in range(self.height):
+            rectangle += (" " * self.x) + ("#" * self.width) + "\n"
+        print(rectangle, end="")
+
+    def update(self, *args, **kwargs):
+        """
+            Updates the arguments props in the class
+        """
+        if len(args) == 0:
+            for key, val in kwargs.items():
+                self.__setattr__(key, val)
+            return
+        try:
+            self.id = args[0]
+            self.width = args[1]
+            self.height = args[2]
+            self.x = args[3]
+            self.y = args[4]
+        except IndexError:
+            pass
+
+    def to_dictionary(self):
+        """
+            Returns a dictionary representation of this class
+        """
+        return {'x': getattr(self, "x"),
+                'y': getattr(self, "y"),
+                'id': getattr(self, "id"),
+                'height': getattr(self, "height"),
+                'width': getattr(self, "width")}
+
+    @staticmethod
+    def setter_validation(attribute, value):
+        if type(value) != int:
+            raise TypeError("{} must be an integer".format(attribute))
+        if attribute == "x" or attribute == "y":
+            if value < 0:
+                raise ValueError("{} must be >= 0".format(attribute))
+        elif value <= 0:
+            raise ValueError("{} must be > 0".format(attribute))
+
+    def __str__(self):
+        return "[Rectangle] ({}) {}/{} - {}/{}".format(self.id, self.x, self.y,
+                                                       self.width, self.height)
