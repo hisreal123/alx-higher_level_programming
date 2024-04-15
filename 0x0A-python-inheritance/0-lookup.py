9e6660d83b6b4cbd0d901f6a8afcf2c9e023c5e8
hisreal123 <omosgoerge@gmail.com>
2024-04-15 17:15:54 -0500

    Fixed first

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
+""" Defines available attributes/methods of an object"""
+
 
 def lookup(obj):
     """ Return a list of object attributes of methods """
-    return (dir(obj))
-
+    return dir(obj)
