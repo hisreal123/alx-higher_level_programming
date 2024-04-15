e13e20428fa93069b25d1a6f1b20ed9b186e8faf
hisreal123 <omosgoerge@gmail.com>
2024-04-15 17:46:58 -0500

    Fixed second

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -1,6 +1,4 @@
 #!/usr/bin/python3
-""" Defines available attributes/methods of an object"""
-
 """ MyList class inheriting from list class"""
 
 
@@ -8,6 +6,5 @@ class MyList(list):
     """" public instance """
 
     def print_sorted(self):
-        if isinstance(list, int):
-            sorted_list = sorted(self)
-            print(sorted_list)
+        sorted_list = sorted(self)
+        print(sorted_list)
