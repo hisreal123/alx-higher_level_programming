115c16e6ea6eec51e8d444a52b6c1fd895e3b99d
hisreal123 <omosgoerge@gmail.com>
2024-04-15 18:38:53 -0500

    Empty class

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -3,14 +3,7 @@
 class that inherited (directly or indirectly)  """
 
 
-def is_same_class(obj, a_class):
-    """Check if an object is an instance of a given class.
-      Args:
-          obj (any): The object to check.
-          a_class (type): The class to compare the type of obj to.
-      Returns:
-          Boolean of an instance of a_class.
-      """
+def inherits_from(obj, a_class):
 
     if issubclass(type(obj), a_class) and type(obj) != a_class:
         return True
