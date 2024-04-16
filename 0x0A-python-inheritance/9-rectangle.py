2c6deaf7302989ffa6f41445d9917f53edc4d63c
hisreal123 <omosgoerge@gmail.com>
2024-04-15 19:31:54 -0500

    Empty class

---
+++ 0x0A-python-inheritance/9-rectangle.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+
+""" Define Geometry module """
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
+
+
+class Rectangle(BaseGeometry):
+    """ Rectangle body """
+
+    def __init__(self, width, height):
+        super().integer_validator("width", width)
+        super().integer_validator("height", height)
+        self.__width = width
+        self.__height = height
+
+    def area(self):
+        """" returns  the area ofo the rectangle """
+        return self.__width * self.__height
+
+
+    def __str__(self):
+        return "[Rectangle] {}/{}".format(self.__width, self.__height)
