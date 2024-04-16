80f6d12ec07b2a9f8cfc63eabf105fd3b1dd5086
hisreal123 <omosgoerge@gmail.com>
2024-04-15 20:01:04 -0500

    Empty class

--- 0x0A-python-inheritance/11-square.py
+++ 0x0A-python-inheritance/11-square.py
@@ -1,11 +1,13 @@
 #!/usr/bin/python3
-
-""" Define square class"""
+"""Defines Rectangle module."""
 Rectangle = __import__('9-rectangle').Rectangle
 
 
 class Square(Rectangle):
+    """class body."""
+
     def __init__(self, size):
+
         self.integer_validator("size", size)
         super().__init__(size, size)
         self.__size = size
