5be7d9ce5e786e6ab74b971f5e483ed638b36457
hisreal123 <omosgoerge@gmail.com>
2024-04-15 19:44:12 -0500

    Empty class

---
+++ 0x0A-python-inheritance/10-square.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+
+""" class square"""
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    def __init__(self, size):
+        super().integer_validator("size", size)
+        self.__size = size
+        # Rectangle.__init__(self, size, size)
+
+    def __str__(self):
+        """returns the string description of the class"""
+        return "[Rectangle] {}/{}".format(self.__size, self.__size)
+
+    def area(self):
+        """" Returning the area of a square"""
+        return self.__size * self.__size
