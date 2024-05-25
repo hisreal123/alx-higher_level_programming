016dc4d04339d31a878d930d109599d41fec4ef3
hisreal123 <omosgoerge@gmail.com>
2024-05-25 01:48:22 +0100

    tast 9

--- 0x0D-SQL_introduction/9-full_creation.sql
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -1,4 +1,13 @@
 -- a script that creates a table second_table in the database in your MySQL server and add multiples rows.
+--creating the table
 CREATE TABLE IF NOT EXISTS`second_table` (`id` INT, `name` VARCHAR(256), `score` INT)
+
+-- insterting multiple row data
 INSERT INTO `second_table` (`id`, `name`, `score`)
-VALUES ((1,2,3,4,5), ('John', 'Alex', 'Bob', 'George'), (10,3,14,8));
+VALUES (
+    (1,'John',10), 
+    (2,'Alex',3), 
+    (3,'Bob',14), 
+    (4,'George',8), 
+    (10,3,14,8)
+);
