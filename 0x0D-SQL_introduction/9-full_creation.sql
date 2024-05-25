35a193e52da7c678cf4ea8d74cdc4d82184201f3
hisreal123 <omosgoerge@gmail.com>
2024-05-25 01:49:51 +0100

    tast 9

--- 0x0D-SQL_introduction/9-full_creation.sql
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -4,9 +4,9 @@ CREATE TABLE IF NOT EXISTS`second_table` (`id` INT, `name` VARCHAR(256), `score`
 
 -- insterting multiple row data
 INSERT INTO `second_table` (`id`, `name`, `score`)
-VALUES (
+VALUES 
     (1,'John',10), 
     (2,'Alex',3), 
     (3,'Bob',14), 
-    (4,'George',8), 
-);
+    (4,'George',8);
+
