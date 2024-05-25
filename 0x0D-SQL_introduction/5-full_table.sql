0e861866f356cd590648a66a3a3d28472407e9bd
hisreal123 <omosgoerge@gmail.com>
2024-05-25 01:07:24 +0100

    tast 6

--- 0x0D-SQL_introduction/5-full_table.sql
+++ 0x0D-SQL_introduction/5-full_table.sql
@@ -1,2 +1,2 @@
--- a script that prints full description of the table from the database;
+-- a script that prints full description of the table from the database task 6;
 SHOW CREATE TABLE first_table;
