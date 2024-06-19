641163b538dc53c5e5d6bc3a77b5905c498a9a95
hisreal123 <omosgoerge@gmail.com>
2024-06-19 16:54:14 -0500

    making the push !!!

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,10 +1,9 @@
 #!/usr/bin/node
-const arg = process.argv.slice(2);
+const arg = process.argv.slice(2).map(Number);
 
-if (!arg) {
-  console.log(0);
-} else if (arg.length < 2) {
+if (arg.length === 0 || arg.length === 1) {
   console.log(0);
 } else {
-  console.log(arg.sort()[arg.length - 2]);
+  const secondlargest = arg.sort((a, b) => b - a)[1];
+  console.log(secondlargest);
 }
