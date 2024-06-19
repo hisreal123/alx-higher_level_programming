870f14d1afccbc24ad5c8e7da156450b51cfacc7
hisreal123 <omosgoerge@gmail.com>
2024-06-19 16:09:00 -0500

    making the push !!!

--- 0x12-javascript-warm_up/10-factorial.js
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -4,15 +4,12 @@ const sysArg = process.argv.slice(2);
 const n = parseInt(sysArg[0]);
 
 const factorial = (num) => {
-  // Base case: factorial of 0 or 1 is 1
   if (num === 0 || num === 1) {
     return 1;
   }
-  // Handle case where num is NaN (not a number)
   if (isNaN(num)) {
     return 1;
   }
-  // Recursive case: num * factorial of (num - 1)
   return num * factorial(num - 1);
 };
 
