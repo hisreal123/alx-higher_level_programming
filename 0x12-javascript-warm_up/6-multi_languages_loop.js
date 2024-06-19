3aa22212f405fd4b72354b042cf9f96b2356214d
hisreal123 <omosgoerge@gmail.com>
2024-06-19 14:02:05 -0500

    making the push !!!

---
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+
+const ArrayOfString = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
+
+// ArrayOfString.forEach((index) => {
+//   console.log(index);
+// });
+for (let i = 0; i <= ArrayOfString.length - 1; i++) {
+  console.log(ArrayOfString[i]);
+}
