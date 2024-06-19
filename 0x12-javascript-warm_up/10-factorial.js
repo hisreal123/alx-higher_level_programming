2a94c139c456cb599fadaaffab6df801df70185e
hisreal123 <omosgoerge@gmail.com>
2024-06-19 16:32:01 -0500

    making the push !!!

--- 0x12-javascript-warm_up/10-factorial.js
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -1,17 +1,13 @@
 #!/usr/bin/node
 
-const sysArg = process.argv.slice(2);
-const n = parseInt(sysArg[0]);
+const n = Number(process.argv.slice(2));
 
 const factorial = (num) => {
-  if (Number.isNaN(num)) {
+  if (Number.isNaN(num) || num === 1) {
     return 1;
+  } else {
+    return num * factorial(num - 1);
   }
-  if (isNaN(num)) {
-    return 1;
-  }
-  return num * factorial(num - 1);
 };
 
-const result = factorial(n);
-console.log(result);
+console.log(factorial(n));
