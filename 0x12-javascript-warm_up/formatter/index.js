f8f2a1e739939b763c1fb99b396f60a138a102eb
hisreal123 <omosgoerge@gmail.com>
2024-06-19 07:43:13 -0500

    newCommit

---
+++ 0x12-javascript-warm_up/formatter/index.js
@@ -0,0 +1,45 @@
+const { exec, spawn } = require("child_process");
+const { stderr } = require("process");
+
+const gitCommit = (commitMessage) => {
+  exec(
+    `git add . && git commit -m ${commitMessage} && git push`,
+    (error, stdout, stderr) => {
+      if (error) {
+        console.error(`Error pushing latest changes ${error.message}`);
+        return;
+      }
+
+      if (stderr) {
+        console.error(`Exec stderr: ${stderr}`);
+        return;
+      }
+
+      console.log(`
+      ================ Successfully Committed ✅  ==============================: 
+      ================ Successfully Committed ✅  ==============================: 
+      ${stdout}`);
+    }
+  );
+};
+const format = (arg1, arg2) => {
+  if (!arg1) return console.log("No arguement passed for formatting !!");
+
+  exec(`semistandard --fix ${arg1} `, (error, stdout, stderr) => {
+    if (error) {
+      console.error(`Error: ${error.message}`);
+      return;
+    }
+
+    if (stderr) {
+      console.error(`Stderr: ${stderr}`);
+      return;
+    }
+
+    console.log(`Stdout: ${stdout} commiting code now  ⏳`);
+    
+    gitCommit(arg2);
+  });
+};
+
+module.exports = format;
---
+++ 0x12-javascript-warm_up/main.js
@@ -0,0 +1,6 @@
+const format = require("./formatter/index");
+
+// get the system arguements
+const arg1 = process.argv[2]
+const arg2 = process.argv[3]
+format(arg1, arg2);
