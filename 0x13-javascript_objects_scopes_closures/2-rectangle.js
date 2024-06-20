8bd2feb26a425161b0954c5d79f200afb156ffa0
hisreal123 <omosgoerge@gmail.com>
2024-06-20 17:23:05 -0500

    making the push !!!

--- 0x13-javascript_objects_scopes_closures/2-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/2-rectangle.js
@@ -2,7 +2,7 @@
 // class Rectangle
 
 class Rectangle {
-  constructor (w, h) {
+  constructor(w, h) {
     if (this._isValidDimension(w) && this._isValidDimension(h)) {
       this.width = w;
       this.height = h;
@@ -11,16 +11,9 @@ class Rectangle {
     }
   }
 
-  _isValidDimension (value) {
+  _isValidDimension(value) {
     return Number.isInteger(value) && value > 0;
   }
 }
 
 module.exports = Rectangle;
-
-// if (Number.isInteger(w) && w > 0 && Number.isInteger(h) && h > 0) {
-//   this.width = w;
-//   this.height = h;
-// } else {
-//   return;
-// }
--- 0x13-javascript_objects_scopes_closures/3-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -1,1 +1,26 @@
 #!/usr/bin/node
+// class Rectangle
+
+class Rectangle {
+  constructor (w, h) {
+    if (this._isValidDimension(w) && this._isValidDimension(h)) {
+      this.width = w;
+      this.height = h;
+    } else {
+      return null;
+    }
+  }
+
+  _isValidDimension (value) {
+    return Number.isInteger(value) && value > 0;
+  }
+
+  // an instance called print
+  print () {
+    for (let i = 0; i < this.height; i++) {
+      console.log('X'.repeat(this.width));
+    }
+  }
+}
+
+module.exports = Rectangle;
