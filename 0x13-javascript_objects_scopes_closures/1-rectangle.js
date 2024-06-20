4739b17cf36d319f86420f57ba0fce00ce33aac3
hisreal123 <omosgoerge@gmail.com>
2024-06-20 17:14:02 -0500

    making the push !!!

--- 0x13-javascript_objects_scopes_closures/1-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/1-rectangle.js
@@ -2,7 +2,7 @@
 // class Rectangle
 
 class Rectangle {
-  constructor(w, h) {
+  constructor (w, h) {
     this.width = w;
     this.height = h;
   }
--- 0x13-javascript_objects_scopes_closures/2-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/2-rectangle.js
@@ -1,1 +1,28 @@
 #!/usr/bin/node
+// class Rectangle
+
+class Rectangle {
+  constructor (w, h) {
+    if (this._isValidDimension(w) && this._isValidDimension(h)) {
+      this.width = w;
+      this.height = h;
+    } else {
+    }
+  }
+
+  _isValidDimension (value) {
+    return Number.isInteger(value) && value > 0;
+  }
+}
+
+module.exports = Rectangle;
+
+
+
+
+  // if (Number.isInteger(w) && w > 0 && Number.isInteger(h) && h > 0) {
+  //   this.width = w;
+  //   this.height = h;
+  // } else {
+  //   return;
+  // }
