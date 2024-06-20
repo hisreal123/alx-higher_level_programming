d8a0d1f01c9bb204060a4a24bbe6050916f99467
hisreal123 <omosgoerge@gmail.com>
2024-06-20 18:02:03 -0500

    multitasking

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -1,1 +1,20 @@
 #!/usr/bin/node
+const Rectangle = require('./4-rectangle');
+
+class Square extends Rectangle {
+  size;
+  constructor (size) {
+    super(size, size);
+    this.size = size;
+  }
+
+  //    prints the rectangle using the c
+  charPrint (c) {
+    const char = c || 'X';
+    for (let i = 0; i < this.height; i++) {
+      console.log(char.repeat(this.width));
+    }
+  }
+}
+
+module.exports = Square;
