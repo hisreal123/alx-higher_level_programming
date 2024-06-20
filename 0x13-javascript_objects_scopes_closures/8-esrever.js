f355430a9e9feb1334551a3cd6bbc9168e85b927
hisreal123 <omosgoerge@gmail.com>
2024-06-20 18:31:18 -0500

    multitasking

--- 0x13-javascript_objects_scopes_closures/8-esrever.js
+++ 0x13-javascript_objects_scopes_closures/8-esrever.js
@@ -1,1 +1,10 @@
 #!/usr/bin/node
+exports.esrever = function (list) {
+  const tempList = [];
+
+  for (let i = list.length - 1; i >= 0; i--) {
+    tempList.push(list[i]);
+  }
+
+  return tempList;
+};
