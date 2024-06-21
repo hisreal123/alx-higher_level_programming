0c81298406a46e7036622bf9f955e5da169e1e26
hisreal123 <omosgoerge@gmail.com>
2024-06-20 19:16:41 -0500

    multitasking

--- 0x13-javascript_objects_scopes_closures/101-sorted.js
+++ 0x13-javascript_objects_scopes_closures/101-sorted.js
@@ -1,2 +1,14 @@
 #!/usr/bin/node
 
+const { dict } = require('./101-data');
+const groupedDict = {};
+
+Object.entries(dict).forEach(([key, value]) => {
+  if (groupedDict[value]) {
+    groupedDict[value].push(key);
+  } else {
+    groupedDict[value] = [key];
+  }
+});
+
+console.log(groupedDict);
