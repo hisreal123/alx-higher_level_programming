083970a1df3d616923068ff1ef270a502326173e
hisreal123 <omosgoerge@gmail.com>
2024-06-22 14:32:30 -0500

    second  mapping

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -1,1 +1,15 @@
-#!/usr/bin/node
+#!/usr/bin/python3
+"""  Write a script that lists all states from the database with a name starting with N """
+import MySQLdb
+import sys
+
+if __name__ == "__main__":
+    db = MySQLdb.connect(host="localhost", user=sys.argv[1],
+                         passwd=sys.argv[2], db=sys.argv[3], port=3306)
+    c = db.cursor()
+    c.execute("SELECT * FROM states WHERE name LIKE N%")
+    rows = c.fetchall()
+    for row in rows:
+        print(row)
+    c.close()
+    db.close()
