7f5e845d24b5d882a6eb7b8dfdd42a0a141dbf33
hisreal123 <omosgoerge@gmail.com>
2024-07-04 18:13:57 -0500

    curl

--- 0x10-python-network_0/101-post_json.sh
+++ 0x10-python-network_0/101-post_json.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Displays only the status code of the response.
-curl -s POST -H -d "Content-Type: application/json" $2 $1
+curl -s POST -H  "Content-Type: application/json" -d "$2" "$1"
