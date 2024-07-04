abf43623671cb8099c2c879154033decdf599376
hisreal123 <omosgoerge@gmail.com>
2024-07-04 18:08:59 -0500

    curl

--- 0x10-python-network_0/101-post_json.sh
+++ 0x10-python-network_0/101-post_json.sh
@@ -2,5 +2,5 @@
 # Displays only the status code of the response.
 URL = $1
 FILENAME = $2
-const response=$(curl -s -X POST -H "Content-Type: application/json" -d @"$FILENAME" "$URL);
+const response=$(curl -s -X POST -H "Content-Type: application/json" -d @"$FILENAME" "$URL");
 echo "$response"
