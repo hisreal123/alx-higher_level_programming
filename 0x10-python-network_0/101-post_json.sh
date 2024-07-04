d4fedd037885b5cab52b4806991c409162744def
hisreal123 <omosgoerge@gmail.com>
2024-07-04 18:10:08 -0500

    curl

--- 0x10-python-network_0/101-post_json.sh
+++ 0x10-python-network_0/101-post_json.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
 # Displays only the status code of the response.
-const response=$(curl -s -X POST -H "Content-Type: application/json" -d $2 $1);
-echo "$response"
+curl -s -X POST -H "Content-Type: application/json" -d $2 $1
