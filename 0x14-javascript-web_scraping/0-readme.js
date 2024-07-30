0fc6a1c6df57c1faba72085b188a8bb019f0397e
hisreal123 <omosgoerge@gmail.com>
2024-07-30 07:51:43 -0500

    web s

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,10 +1,8 @@
 #!/usr/bin/node
-
 // a script that reads and prints the content of a file.
-const fs = require("fs");
-const path = require("path");
+const fs = require('fs');
 
-fs.readFile(process.argv[2], "utf-8", (err, data) => {
+fs.readFile(process.argv[2], 'utf-8', (err, data) => {
   if (err) {
     console.log(err);
   } else {
--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -1,9 +1,10 @@
 #!/usr/bin/node
 // a script to write a string into a file
 
-const fs = require("fs");
-const path = require("path");
+const fs = require('fs');
 
-fs.writeFile(process.argv[2], "Python is cool", "utf8", (err) =>
-  err ? console.log(err) : null
-);
+fs.writeFile(process.argv[2], 'Python is cool', 'utf8', (err) => {
+  if (err) {
+    console.log(err);
+  }
+});
