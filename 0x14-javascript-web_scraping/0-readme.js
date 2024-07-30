96a5e3f0fd5722ac712f7c9e1742308a903c7d0c
hisreal123 <omosgoerge@gmail.com>
2024-07-30 03:50:54 -0500

    web scrapping

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,13 +1,12 @@
 #!/usr/bin/node
 
 // a script that reads and prints the content of a file.
-const fs = require('fs')
-const path = require('path')
-
+const fs = require("fs");
+const path = require("path");
 
 fs.readFile(process.argv[2], "utf-8", (err, data) => {
-  if (err){
-    console.log(err)
+  if (err) {
+    console.log(err);
   } else {
     console.log(data);
   }
