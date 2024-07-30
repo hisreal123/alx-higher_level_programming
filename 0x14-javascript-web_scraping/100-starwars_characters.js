2f869f7cdf801007ce190cec3a14609b0591a272
hisreal123 <omosgoerge@gmail.com>
2024-07-30 11:15:18 -0500

    web s

--- 0x14-javascript-web_scraping/100-starwars_characters.js
+++ 0x14-javascript-web_scraping/100-starwars_characters.js
@@ -4,14 +4,14 @@
 const req = require("request");
 const id = process.argv[2];
 const url = "https://swapi-api.hbtn.io/api/films/";
-req.get(url + id, function (error, res, body) {
+req.get(url + id, function (error, _res, body) {
   if (error) {
     console.log(error);
   }
   const data = JSON.parse(body);
   const dd = data.characters;
   for (const i of dd) {
-    req.get(i, function (error, res, body1) {
+    req.get(i, function (error, _res, body1) {
       if (error) {
         console.log(error);
       }
