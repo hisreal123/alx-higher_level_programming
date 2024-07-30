1a2b51f4a86dedb53d172b6ffe08ef226a5c3170
hisreal123 <omosgoerge@gmail.com>
2024-07-30 08:25:40 -0500

    web s

---
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -0,0 +1,7 @@
+#!/usr/bin/node
+// A script that display the status code of a GET request.
+
+const request = require('request');
+request.get(process.argv[2], (err, response) => {
+  err ? console.log(err) : console.log('code:' + response.statusCode);
+});
